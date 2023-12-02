0542b81ad59a2de8d3b7243ec809d8f62c4ac19f
Daniel Santana <daniel.sasilva@pagar.me>
2023-12-02 18:56:25 -0300

    precommit husky and lint staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Lint and Pretify TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `yarn lint --fix .${filenames.join('--file')}`
+  ]
+}
