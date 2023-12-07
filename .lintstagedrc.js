81f2cf4665718cf358f8bc4ea27f1dd480cc3d90
Daniel Santana <daniel.sasilva@pagar.me>
2023-12-07 02:07:11 -0300

    initial config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   // Lint and Pretify TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `yarn lint --fix . ${filenames.join('--file')}`
+    `yarn lint --fix . ${filenames.join('--file')}`,
+    `yarn test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/app/**', // Should be tested in e2e
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/**/stories.tsx',
+    '!src/styles/**'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
@@ -12,5 +19,12 @@ module.exports = {
         presets: ['next/babel']
       }
     ]
+  },
+  // https://github.com/styled-components/styled-components/issues/4081
+  // v6 of styled-components doesn't inject styles in test environment
+  // we should to force it to use the browser version
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
   }
 }
